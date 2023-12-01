4d7a4e41df27ae7d034ca7a9648942dc936d5115
Johnnnex <onyeiborjohn05@gmail.com>
2023-12-01 21:20:33 +0100

    Prod ready

--- pages/page/the-faith.jsx
+++ pages/page/the-faith.jsx
@@ -1,10 +1,21 @@
 import HeadComp from '@/layout/headcomp'
-import React from 'react'
+import { Inter } from 'next/font/google'
+import { useTranslation } from 'react-i18next'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const TheFaith = () => {
   return (
     <>
-        <HeadComp title=""
+        <HeadComp title="Tell | The Faith" />
+        <main className={`${inter.className} pt-[190px]`}>
+            <section className="flex w-[85%] mx-auto">
+                <figure className="h-[408px] rounded-[8px] w-[30%] max-w-[420px]">
+
+                </figure>
+                <div className=""></div>
+            </section>
+        </main>
     </>
   )
 }
